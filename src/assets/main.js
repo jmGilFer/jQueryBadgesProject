ec91b42ff0a606f07d2cbe72e9a2058f883cd7c5
Dani 'CHaNG <changete@gmail.com>
2017-08-19 11:37:49 +0200

    colors

--- src/assets/main.js
+++ src/assets/main.js
@@ -36,15 +36,38 @@ $(function () {
         $badges.append("<h2>User badges</h2>");
         $badges.append(`<div id='user-badges'></div>`);
         const $userBadges = $badges.find("#user-badges");
+        const badgeGroup = [];
         response.badges.sort((badgeA, badgeB) => {
-          return badgeA.name < badgeB.name ? -1 : 1;
+          return !badgeA.course_url ? 1 : !badgeB.course_url ? -1 :
+            badgeA.course_url < badgeB.course_url ? -1 : 1;
         }).map((badge) => {
-          $userBadges.append(`<div class='user-badge' title="${badge.name}" alt="${badge.name}">
+          if (!badgeGroup[badge.course_url]) {
+            badgeGroup[badge.course_url] = randomHSL();
+          }
+          let groupColor = badgeGroup[badge.course_url];
+          const $newEl = $(`${badge.course_url ? `<a class="linked-badge" href=${badge.course_url}>` : ""}
+          <div style="background:${groupColor.bg}; border: 1px solid ${groupColor.border}" class='user-badge ${badge.course_url ? `badge-group` : ""}' title="${badge.name}" alt="${badge.name}">
             <img src="${badge.badge}" title="${badge.name}" alt="${badge.name}" />
             <p>${badge.name}</p>
-          </div>`);
+          </div>
+          ${badge.course_url ? `</a>` : ""}`);
+          if (badge.course_url) {
+            $newEl.append()
+          }
+          $userBadges.append($newEl);
+
         });
       }
     }
   });
+
+  function randomHSL() {
+    const h = 360 * Math.random();
+    const s = (25 + 70 * Math.random());
+    const l = (85 + 10 * Math.random());
+    return {
+      bg: `hsl(${h}, ${s}%, ${l}%)`,
+      border: `hsl(${h}, ${s}%, ${l - 10}%)`
+    };
+  }
 });
