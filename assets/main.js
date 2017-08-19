43a6b4a89f24872e80447405b9109e632a37999f
Dani 'CHaNG <changete@gmail.com>
2017-08-19 10:21:47 +0200

    ic

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/CHaNGeTe.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      const $badges = $("#badges");
+      response.courses.completed.map((course) => {
+        $badges.append(`<div class='course'>
+          <h3>${course.title}</h3>
+          <img src="${course.badge}" />
+          <a target="_blank" href="${course.url}" class="btn btn-primary">See Course</a>
+        </div>`);
+      });
+    }
+  });
 
 });
